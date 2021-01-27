ba2c7c1bd84eb590ca59e3465e7aa340dbce0706
studyingpanda <chenwenyang1990@163.com>
2021-01-27 09:08:41 +0800

    chen

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">首页</router-link> |
+      <router-link to="/about">关于</router-link> |
+      <router-link to="/layoutDemo">布局演示</router-link>
+    </div>
+    <el-divider></el-divider>
+    <router-view />
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    text-decoration: none;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>Element 演示项目</h1>
+  </div>
+</template>
